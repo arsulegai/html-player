6c213f9f434936695f25ae19c2e60238bba378cc
S m, Aruna <arun.s.m.cse@gmail.com>
2020-04-12 11:45:05 +0530

    Compose notes, remove, add keys
    Signed-off-by: S m, Aruna <arun.s.m.cse@gmail.com>

--- static/scripts/js/button.js
+++ static/scripts/js/button.js
@@ -1,7 +1,29 @@
 var getNote = function() {
     var key = jQuery(this.innerHTML).text();
     var note = key.replace("#", "sharp");
-    alert(eval(note.toLowerCase()));
+
+    var notesection = document.getElementById('notes');
+    notesection.innerText = notesection.innerText + " " + key;
+    // alert(eval(note.toLowerCase()));
+}
+
+var removeNoteFromNotes = function() {
+    var notesection = document.getElementById('notes');
+    var presentText = notesection.innerText;
+    var notes = presentText.split(" ");
+    notes.pop();
+    notesection.innerText = notes.join(" ");
+}
+
+var removeNotes = function() {
+    var notesection = document.getElementById('notes');
+    notesection.innerText = "";
+}
+
+var playTrackNotes = function() {
+    var notesection = document.getElementById('play');
+    var notes = notesection.innerText;
+    // Construct a 2D array, first value is a frequency and second one is a time
 }
 
 window.onload = function() {
@@ -10,4 +32,13 @@ window.onload = function() {
     for (var i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', getNote, false);
     }
+
+    var deleteNotes = document.getElementById('delete');
+    deleteNotes.addEventListener('click', removeNotes, false);
+
+    var removeNote = document.getElementById('remove');
+    removeNote.addEventListener('click', removeNoteFromNotes, false);
+
+    var playNotes = document.getElementById('play');
+    playNotes.addEventListener('click', playTrackNotes, false)
 }
